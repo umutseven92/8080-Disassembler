1a0c29cba2413fb589f9fa45a43cec4367ad3471
Tugcan Olgun <olgunt@itu.edu.tr>
2017-10-19 19:18:55 +0300

    syn

--- src/dis_helper.py
+++ src/dis_helper.py
@@ -5,7 +5,7 @@ def get_dest(offset, hexed, addt, x):
 	dest = ""
 
 	for i in range(1, offset+1):
-		offset_byte = hexed[(x*2) + i*2] + hexed[(x*2) + i*2 +1]
+		offset_byte = hexed[(x*2) + i*2] + hexed[(x * 2) + i * 2 + 1]
 		dest = offset_byte + dest
 
 	dest = addt + dest
